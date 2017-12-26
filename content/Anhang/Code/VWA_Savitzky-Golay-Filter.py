7a65c42d66f4e53fd88180704b01e0e80b1298e9
ProgKlui <florian.kluibenschedl@live.de>
2017-12-26 22:22:57 +0100

    Started Appendix. Added used code listings.
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   Vorwort.aux
    	modified:   abstract.aux
    	new file:   content/Anhang/Code/Anhang_Code.tex
    	new file:   content/Anhang/Code/VWA_Analyse_LC-ESI-MS.py
    	new file:   content/Anhang/Code/VWA_Savitzky-Golay-Filter.py
    	new file:   content/Anhang/Code/VWA_generate_mz_diagrams.py
    	new file:   content/Anhang/ESIMS/Bo-DNCC/619CID-619savgol.png
    	new file:   content/Anhang/ESIMS/Bo-DNCC/619CID40-575CID-575savgol.png
    	new file:   content/Anhang/ESIMS/Bo-DNCC/619CID40-575CID100-452CID-452savgol.png
    	new file:   content/Anhang/ESIMS/Bo-DNCC/619PQD-619savgol.png
    	new file:   content/Anhang/ESIMS/Bo-DYCC/617CID-617-1savgol.png
    	new file:   content/Anhang/ESIMS/Bo-DYCC/617CID-617-2savgol.png
    	new file:   content/Anhang/ESIMS/Bo-DYCC/617CID100-573CID-573savgol.png
    	new file:   content/Anhang/ESIMS/Bo-DYCC/617PQD-617-2savgol.png
    	new file:   content/Anhang/ESIMS/Bo-NCC-1/793CID-793savgol.png
    	new file:   content/Anhang/ESIMS/Bo-NCC-1/793PQD-793savgol.png
    	new file:   content/Anhang/ESIMS/Bo-NCC-1/793PQD-793savgolv5.png
    	new file:   content/Anhang/ESIMS/Bo-NCC-3/647CID-647savgol.png
    	new file:   content/Anhang/ESIMS/Bo-NCC-3/647PQD-647savgol.png
    	new file:   content/Anhang/ESIMS/RP_Bo-DNCC/633CID-633savgol.png
    	new file:   content/Anhang/ESIMS/RP_Bo-DNCC/633CID100-510CID-510savgol.png
    	new file:   content/Anhang/ESIMS/RP_Bo-DNCC/633CID100-510CID100-337CID-337savgol.png
    	new file:   content/Anhang/ESIMS/RP_Bo-DNCC/633PQD-633savgol-v15.png
    	new file:   content/Anhang/ESIMS/RP_Bo-DNCC/633PQD-633savgol.png
    	new file:   content/Anhang/ESIMS/RP_Bo-DNCC/633PQD35-460CID-460savgol.png
    	new file:   content/Anhang/ESIMS/RP_Bo-DNCC/633PQD35-460CID-460savgolv15.png#
    	new file:   content/Anhang/ESIMS/RP_Bo-DYCC/631CID-631-1savgol.png
    	new file:   content/Anhang/ESIMS/RP_Bo-DYCC/631CID-631-2savgol.png
    	new file:   content/Anhang/ESIMS/RP_Bo-DYCC/631PQD-631savgol.png
    	new file:   content/Anhang/ESIMS/RP_Bo-NCC-3/661CID-661-1savgol.png
    	new file:   content/Anhang/ESIMS/RP_Bo-NCC-3/661CID-661-1savgolv20.png
    	new file:   content/Anhang/ESIMS/RP_Bo-NCC-3/661CID-661-2savgol.png
    	new file:   content/Anhang/ESIMS/RP_Bo-NCC-3/661PQD-661-1savgol.png
    	new file:   content/Anhang/ESIMS/RP_Bo-NCC-3/661PQD-661-1savgolv10.png
    	new file:   content/Anhang/ESIMS/RP_Bo-NCC-3/661PQD40-460CID-460savgol.png
    	new file:   content/Anhang/ESIMS/RP_Bo-NCC-3/661PQD40-460CID-460savgolv25.png
    	new file:   content/Anhang/ESIMS/RP_Bo-NCC-3/661PQD40-506CID-506savgol.png
    	new file:   content/Anhang/ESIMS/RP_Bo-NCC-3/661PQD40-506CID100-337CID-337savgol.png
    	new file:   content/Anhang/MSLeafspray/RP_667/667CID-667savgol.png
    	new file:   content/Anhang/MSLeafspray/RP_667/667CID-667savgolv10.png
    	new file:   content/Anhang/MSLeafspray/RP_667/VWA_MS_LeafSpray_667.png
    	new file:   content/Anhang/MSLeafspray/RP_Bo-DNCC/671CID-671savgol.png
    	new file:   content/Anhang/MSLeafspray/RP_Bo-DNCC/671CID-671savgolv20.png
    	new file:   content/Anhang/MSLeafspray/RP_Bo-DNCC/VWA_MS_LeafSpray_671.png
    	new file:   content/Anhang/MSLeafspray/RP_Bo-DYCC/697CID-697savgol.png
    	new file:   content/Anhang/MSLeafspray/RP_Bo-DYCC/697CID-697savgolv2.5.png
    	modified:   main.aux
    	modified:   main.ist
    	modified:   main.lof
    	modified:   main.log
    	modified:   main.pdf
    	new file:   main.py
    	modified:   main.synctex.gz
    	modified:   main.tex
    	modified:   main.toc

---
+++ content/Anhang/Code/VWA_Savitzky-Golay-Filter.py
@@ -0,0 +1,83 @@
+
+# coding: utf-8
+
+# # Savitzky-Golay Filter for smoothing Fragmentation-diagrams
+
+# Function in which savitzky-golay filter is applied:
+
+def savgolay_filter_catabolites(ms_data, ms_name, mode_value, mode):
+    np.set_printoptions(precision=2)  
+    ms_savgol = savgol_filter(ms_data, 23, 6, mode=mode_value) # apply sav-gol filter
+    
+    area_initial = trapz(ms_data, dx=5)
+    area_savgol = trapz(ms_savgol, dx=5)
+    
+    # x_values = np.array([0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100])
+    x_values = np.array([0,5,10,15,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100])
+    
+    ax = plt.axes()
+    ax.set_xlim([0, 100])
+    ax.set_ylim([0, 105])
+    ax.set_xticks(x_values)
+        
+    if(mode == 'initial'):                
+        line = plt.plot(x_values, ms_data, label = i)
+    elif(mode == 'initial-savgol'):
+        line1 = plt.plot(x_values, ms_data, label = i + ' initial, area: ' + str(round(area_initial)))
+        line2 = plt.plot(x_values, ms_savgol, label = i + ' savgol, area: ' + str(round(area_savgol)))
+    elif(mode == 'savgol'):
+        line = plt.plot(x_values, ms_savgol, label = i)
+        
+        # Following code could be used for SVM/SVR but is not "smoothing" function
+        # svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
+        # y_rbf = svr_rbf.fit(x_values.reshape(-1, 1), np.array(ms_data)).predict(x_values.reshape(-1, 1))
+        # plt.plot(x_values, y_rbf, color='navy',  label='RBF model')
+        
+    plt.xlabel('Normalised Collision Energy (%)')
+    plt.ylabel('% of highest peak')
+    plt.legend()
+    ax.yaxis.grid()
+    
+    return ms_savgol
+
+
+# Import needed modules:
+
+import numpy as np
+from numpy import trapz
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.signal import savgol_filter
+
+from sklearn.svm import SVR # import support-vector machine
+
+
+data = pd.io.parsers.read_csv("csv/Kuerbis_Analyse_Bindungskinetik_Fragmente873_Version1.csv")
+
+
+# Specify dataset on which to apply the filter:
+
+catabolite = input("Specify fragmentation path: ")
+fragmentation_mode = input("Specify fragmentation mode: ")
+
+catabolites_string = input("Specify [M]-fragments of above catabolite: ")
+catabolites = catabolites_string.split(",")
+print(catabolites)
+
+
+# Call above defined function and print output:
+
+specify_diagrams = ['initial', 'initial-savgol', 'savgol']
+
+for x in specify_diagrams:
+    for i in catabolites:
+        value = data[[i]].values
+        diag = np.array(value[3:23].astype(np.int))
+        value_new = savgolay_filter_catabolites(np.hstack(diag),i,'nearest', x)
+        
+    plt.title(catabolite+fragmentation_mode+' '+x)
+    
+    diag_name = 'Diagrams_output/'+catabolite+fragmentation_mode+'-'+catabolites[0]+x
+    plt.savefig(diag_name+'.png')
+    plt.show()
+
